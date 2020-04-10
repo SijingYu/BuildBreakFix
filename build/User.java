c2010d6cc14a4d7d6b807a93b2c22e156b403449
SijingYu <51205048+SijingYu@users.noreply.github.com>
2020-04-10 00:24:40 -0400

    Files building the system

---
+++ build/User.java
@@ -0,0 +1,252 @@
+//package bi2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+
+public class User {
+	String name;
+	String pw;
+	
+	Map<Variable, int[]> rights;
+	Map<Rule, int[]> rule_rights;
+	
+	public User(String name, String pw) {
+		this.name = name;
+		this.pw = pw;
+		
+		this.rights = new HashMap<Variable, int[]>();
+		this.rule_rights = new HashMap<Rule, int[]>();
+	}
+	
+	public User(Map<Variable, int[]> rights, Map<Rule, int[]> rule_rights, String name, String pw) {
+		this.rights = rights;
+		this.rule_rights = rule_rights;
+		this.name = name;
+		this.pw = pw;
+	}
+	
+	
+	
+	public void add_right(Variable var, Reader.Right right) {
+		// indexes correspond: 
+		// 0:Read 1:Write 2:Delegate 3:Toggle
+		int[] r = {0,0,0,0};
+		boolean exists = false;
+		
+		if (rights.keySet().contains(var)) {
+			r = rights.get(var);
+			exists = true;
+		}
+		if (right == Reader.Right.READ) {
+			r[0] = 1;
+		} else if (right == Reader.Right.WRITE) {
+			r[1] = 1;
+		} else if (right == Reader.Right.DELEGATE) {
+			r[2] = 1;
+		} else if (right == Reader.Right.TOGGLE) {
+			r[3] = 1;
+		}
+		if (exists) {
+			rights.replace(var, r);
+		} else {
+			rights.put(var, r);
+		}
+		//System.out.println(rightString());
+	}
+	
+	public void User_copy(User u) {
+		this.rights = u.rights;
+		this.name = u.name;
+		this.pw = u.pw;
+		this.rule_rights = u.rule_rights;
+	}
+	
+	public void copyRules(User u) {
+		this.rights = u.rights;
+		this.rule_rights = u.rule_rights;
+	}
+	
+	public boolean hasRight(Variable var, Reader.Right right) {
+		boolean out = false;
+		Set<Variable> keys = this.rights.keySet();
+		Iterator<Variable> iter = keys.iterator();
+		while (iter.hasNext()) {
+			Variable curr = iter.next();
+			if (curr.name.equals(var.name)){
+				int[] r = rights.get(curr);
+				if (right == Reader.Right.READ && r[0] == 1) {
+					out = true;
+				} else if (right == Reader.Right.WRITE && r[1] == 1) {
+					out = true;
+				} else if (right == Reader.Right.DELEGATE && r[2] == 1) {
+					out = true;
+				} else if (right == Reader.Right.TOGGLE && r[3] == 1) {
+					out = true;
+				}
+			}
+		}
+		return out;
+	}
+	
+	public void add_all_rights(Variable var) {
+		int[] r = {1, 1, 1, 1};
+		boolean exists = false;
+		if (rights.keySet().contains(var)) {
+			exists = true;
+		}
+		if (exists) {
+			rights.replace(var, r);
+		} else {
+			rights.put(var, r);
+		}
+		//System.out.println(rightString());
+	}
+	
+	public void rule_add_right(Rule rule, Reader.Right right) {
+		// indexes correspond:
+		// 0:Read 1:Write 2:Delegate 3:Toggle
+		int[] r = {0,0,0,0};
+		boolean exists = false;
+		if (rule_rights.keySet().contains(rule)) {
+			r = rule_rights.get(rule);
+			exists = true;
+		}
+		if (right == Reader.Right.READ) {
+			r[0] = 1;
+		} else if (right == Reader.Right.WRITE) {
+			r[1] = 1;
+		} else if (right == Reader.Right.DELEGATE) {
+			r[2] = 1;
+		} else if (right == Reader.Right.TOGGLE) {
+			r[3] = 1;
+		}
+		if (exists) {
+			rule_rights.replace(rule, r);
+		} else {
+			rule_rights.put(rule, r);
+		}
+		//System.out.println(rule_rightString());
+	}
+	
+	public boolean rule_hasRight(Rule rule, Reader.Right right) {
+		boolean out = false;
+		Set<Rule> keys = this.rule_rights.keySet();
+		Iterator<Rule> iter = keys.iterator();
+		while (iter.hasNext()) {
+			Rule curr = iter.next();
+			if (curr.name.equals(rule.name)){
+				int[] r = rule_rights.get(curr);
+				if (right == Reader.Right.READ && r[0] == 1) {
+					out = true;
+				} else if (right == Reader.Right.WRITE && r[1] == 1) {
+					out = true;
+				} else if (right == Reader.Right.DELEGATE && r[2] == 1) {
+					out = true;
+				} else if (right == Reader.Right.TOGGLE && r[3] == 1) {
+					out = true;
+				}
+			}
+		}
+		return out;
+	}
+	
+	public void rule_add_all_rights(Rule rule) {
+		int[] r = {1, 1, 1, 1};
+		boolean exists = false;
+		if (rule_rights.keySet().contains(rule)) {
+			exists = true;
+		}
+		if (exists) {
+			rule_rights.replace(rule, r);
+		} else {
+			rule_rights.put(rule, r);
+		}
+		System.out.println(rule_rightString());
+	}
+	
+	public String rule_rightString() {
+		String out = this.name + " ";
+		Set<Rule> keys = this.rule_rights.keySet();
+		Iterator<Rule> iter = keys.iterator();
+		while (iter.hasNext()) {
+			Rule curr = iter.next();
+			out += curr.name;
+			int[] r = rule_rights.get(curr);
+			if (r[0] == 1) {
+				out += " READ";
+			}
+			if (r[1] == 1) {
+				out += " WRITE";
+			}
+			if (r[2] == 1) {
+				out += " DELEGATE";
+			}
+			if (r[3] == 1) {
+				out += " TOGGLE";
+			}
+			//out += " " + r.get(i).toString();
+			out += ", ";
+		}
+		return out;
+	}
+	
+	public void removeLocals(ArrayList<Variable> locals) {
+		for (int i = 0; i < locals.size(); i++) {
+			if (this.rights.containsKey(locals.get(i))) {
+				this.rights.remove(locals.get(i));
+			}
+		}
+	}
+	
+	public void del_right(Variable var, Reader.Right right) {
+		// indexes correspond:
+		// 0:Read 1:Write 2:Delegate 3:Toggle
+		int[] r = {0,0,0,0};
+
+		r = rights.get(var);
+		if (right == Reader.Right.READ) {
+			r[0] = 0;
+		} else if (right == Reader.Right.WRITE) {
+			r[1] = 0;
+		} else if (right == Reader.Right.DELEGATE) {
+			r[2] = 0;
+		} else if (right == Reader.Right.TOGGLE) {
+			r[3] = 0;
+		}
+		rights.replace(var, r);
+		//System.out.println(rightString());
+	}
+
+	
+	public String rightString() {
+		String out = this.name + " ";
+		Set<Variable> keys = this.rights.keySet();
+		Iterator<Variable> iter = keys.iterator();
+		
+		while (iter.hasNext()) {
+			Variable curr = iter.next();
+			out += "NAME";
+			out += curr.name;
+			
+			int[] r = rights.get(curr);
+			if (r[0] == 1) {
+				out += " READ";
+			}
+			if (r[1] == 1) {
+				out += " WRITE";
+			}
+			if (r[2] == 1) {
+				out += " DELEGATE";
+			}
+			if (r[3] == 1) {
+				out += " TOGGLE";
+			}
+			out += ", ";
+		}
+		return out;
+	}
+}
---
+++ build/src/bi2/Evaluator.java
@@ -0,0 +1,134 @@
+//package bi2;
+import java.util.ArrayList;
+
+public class Evaluator {
+	public static double mean(ArrayList<Integer> ints) {
+		if (ints.size() != 0) {
+			int curr = 0;
+			for (int i = 0; i < ints.size(); i++) {
+				curr += ints.get(i);
+			}
+			return curr/ints.size();
+		}
+		return -1;
+	}
+	public static int max(ArrayList<Integer> ints) {
+		int curr = -1;
+		for (int i = 0; i < ints.size(); i++) {
+			if (ints.get(i) > curr) {
+				curr = ints.get(i);
+			}
+		}
+		return curr;
+	}
+	public static int min(ArrayList<Integer> ints) {
+		int curr = Integer.MAX_VALUE;
+		for (int i = 0; i < ints.size(); i++) {
+			if (ints.get(i) < curr) {
+				curr = ints.get(i);
+			}
+		}
+		return curr;
+	}
+	public static String math_result (String func, Variable x, int i, int j, boolean four) {
+		ArrayList<Integer> vals = x.value;
+		ArrayList<Integer> sub;
+		if (four) {
+			sub = sublist(x.value, i, j);
+			if (i > vals.size() | j > vals.size() | i < 0 | j < 0 | i <= j) {
+				return "FAILED";
+			}
+		}
+		sub = x.value;
+		if (func.equals("mean")) {
+			return Double.toString(mean(sub));
+		}else if (func.equals("min")) {
+			return Integer.toString(min(sub));
+		}else if (func.equals("max")) {
+			return Integer.toString(max(sub));
+		}else if (func.equals("count")) {
+			return Integer.toString(count(sub));
+		}
+		return "";
+	}
+	public static ArrayList<Integer> sublist (ArrayList<Integer> list, int i, int j){ // i <= j
+		ArrayList<Integer> sub = new ArrayList<Integer>();
+		for (int k = i; k <= j; k++) {
+			sub.add(list.get(k));
+		}
+		return sub;
+	}
+	public static int count(ArrayList<Integer> value) {
+		return value.size();
+	}
+
+	public static String eval(String s) {
+		//System.out.println(s);
+		String[] spl = s.trim().split(" ");
+		if (spl.length == 1) { // value: x or i or x.i or
+			if (spl[0].matches("d[-+]?\\d+(\\.\\d+)?")) { //value:i
+				return s;
+			}else if (s.contains(".")){ //value: x.i
+				String[] spl2 = s.trim().split("\\.");
+				//int index = Integer.parseInt(spl2[1]);
+				int index = Integer.parseInt(eval(spl2[1]));
+				
+				int varIndex = Reader.getVarIndex(spl2[0]);
+				int locIndex = Reader.getLocalIndex(spl2[0]);
+				
+				if (varIndex != -1) {
+					return Integer.toString(Reader.getVariable(spl2[0]).value.get(index));
+				} else if (locIndex != -1) {
+					return Integer.toString(Reader.getLocal(spl2[0]).value.get(index));
+				}
+			} else if (spl[0].matches("-?\\d+(\\.\\d+)?")) { // returns a literal number
+				return spl[0];
+			} else if (spl[0].matches("[\\w]+")) { // returns the value of x
+				int varIndex = Reader.getVarIndex(spl[0]);
+				int locIndex = Reader.getLocalIndex(spl[0]);
+				if (varIndex != -1) {
+					return Integer.toString(Reader.getVariable(spl[0]).value.get(0));
+				} else if (locIndex != -1) {
+					return Integer.toString(Reader.getLocal(spl[0]).value.get(0));
+				}
+			} else {
+				return "FAILED";
+			}
+		}else if (spl.length == 2) { // func x
+			String func = spl[0];
+			Variable var = Reader.getVariable(spl[1]);
+			return math_result(func, var, 0, 0, false);
+		}else if (spl.length == 3) { // X + Y
+			int val1;
+			int val2;
+			if (!spl[0].matches("-?\\d+(\\.\\d+)?")) {
+				val1 = Integer.parseInt(eval(spl[0]));
+			} else {
+				val1 = Integer.parseInt(spl[0]);
+			}
+			
+			if (!spl[2].matches("-?\\d+(\\.\\d+)?")) {
+				val2 = Integer.parseInt(eval(spl[2]));
+			} else {
+				val2 = Integer.parseInt(spl[2]);
+			}
+			
+			if (s.contains("+")) {
+				return Integer.toString(val1+val2);
+			}else if (s.contains("-")) {
+				return Integer.toString(val1-val2);
+			}else if (s.contains("*")) {
+				return Integer.toString(val1*val2);
+			}else if (s.contains("/")) {
+				return Double.toString(val1/val2);
+			}
+		}else if (spl.length == 4) {
+			String func = spl[0];
+			Variable var = Reader.getVariable(spl[1]);
+			int i = Integer.parseInt(spl[2]);
+			int j = Integer.parseInt(spl[3]);
+			return math_result(func, var, j, i, false);
+		}
+		return "FAILED";
+	}
+}
